239a4e5ce933b79fb8e8dd0d7011bb307b45b8d3
Tushant Mehta <000435076@student.vcc.ca>
2020-11-16 23:57:19 -0800

    added files for edit profile feature

---
+++ inc/utilities/alreadyRegistered.class.php
@@ -0,0 +1,29 @@
+<?php
+
+class AlreadyRegistered{
+
+
+    static public function html_Header() {
+
+        echo "<!DOCTYPE html>
+        <html lang='en'>
+        <head>
+        
+
+        </head>
+        <body>
+        
+        <p>Already Registered <a href='./loginPage.php'>click here</a> to login</p>";
+    }
+
+
+
+    static public function html_Footer() {
+
+        echo "</body>
+            </html>";
+    }
+}
+
+
+?>
